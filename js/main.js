17cb53ed4d7a97374c58f512feb002629e6f333f
Xavi <xaviher007@hotmail.com>
2018-12-27 19:11:33 +0100

    searchBar
    working

--- js/main.js
+++ js/main.js
@@ -175,3 +175,23 @@ function goBack(){
 
   document.getElementById("coin_info").innerHTML = "";
 }
+
+function searchCoin() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("all_coins");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
