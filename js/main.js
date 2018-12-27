58ecd6a7dbd4e29678d06ef113dc63a0c331ac8e
Xavi <xaviher007@hotmail.com>
2018-12-27 19:03:15 +0100

    barra izquierda
    botones, precio al pulsar, etc

--- js/main.js
+++ js/main.js
@@ -1,59 +1,69 @@
-var checkTime;
-
 //Initialize function
+var focusable1;
+var coins;
+
 var init = function() {
   // TODO:: Do your initialization job
-  console.log('init() called');
   $.ajaxSetup({
     'cache': true
   });
-  getPrices($);
+  getPrices();
   bestCoins();
-
-  document.addEventListener('visibilitychange', function() {
-    if (document.hidden) {
-      // Something you want to do when hide or exit.
-    } else {
-      // Something you want to do when resume.
-    }
-  });
-
-  // add eventListener for keydown
-  var focusable = document.querySelectorAll("a.btn");
+  document.getElementById("coin_info").style.display = "none";
   var i = 0;
-
+  var j = 0;
+  // add eventListener for keydown
   document.addEventListener('keydown', function(e) {
     switch (e.keyCode) {
       case 37: //LEFT arrow
-      if (i > 0) {
-    	i--;
-        document.getElementById(focusable[i].id).focus();
-        console.log(i);
+        if (i > 0) {
+          i--;
+          document.getElementById(focusable1[i].id).focus();
+          console.log(i);
 
-      } else {
-        i = focusable.length - 1;
-        document.getElementById(focusable[i].id).focus();
-        console.log(i);
-      }
+        } else {
+          i = focusable1.length - 1;
+          document.getElementById(focusable1[i].id).focus();
+          console.log(i);
+        }
         break;
       case 38: //UP arrow
-        break;
-      case 39: //RIGHT arrow
-      if (i < focusable.length - 1) {
-    	i++;
-        document.getElementById(focusable[i].id).focus();
-        console.log(i);
+        if (j >0) {
+        j--;
+        document.getElementById(focusable1[j].id).focus();
+        console.log(j);
 
       } else {
-        i = 0;
-        document.getElementById(focusable[i].id).focus();
-        console.log(i);
+        j = focusable1.length - 1;
+        document.getElementById(focusable1[j].id).focus();
+        console.log(j);
       }
         break;
+      case 39: //RIGHT arrow
+        if (i < focusable1.length - 1) {
+          i++;
+          document.getElementById(focusable1[i].id).focus();
+          console.log(i);
+
+        } else {
+          i = 0;
+          document.getElementById(focusable1[i].id).focus();
+          console.log(i);
+        }
+        break;
       case 40: //DOWN arrow
-                break;
+        if (j < focusable1.length - 1) {
+          j++;
+          document.getElementById(focusable1[j].id).focus();
+          console.log(j);
+        } else {
+          j = 0;
+          document.getElementById(focusable1[j].id).focus();
+          console.log(i);
+        }
+        break;
       case 13: //OK button
-        document.getElementById(focusable[i].id).click();
+        document.getElementById(focusable1[i].id).click();
         break;
       case 10009: //RETURN button
         tizen.application.getCurrentApplication().exit();
@@ -77,10 +87,6 @@ function infoCoin() {
     },
     success: function(response) {
       console.log('request succed');
-      var node = document.createElement("p");
-      var textnode = document.createTextNode(response.data[1].name);
-      node.appendChild(textnode);
-      document.getElementById("info").appendChild(node);
     },
     error: function() {
       console.log("error");
@@ -88,7 +94,7 @@ function infoCoin() {
   });
 }
 
-function bestCoins(){
+function bestCoins() {
   $.get({
     url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
     dataType: 'json',
@@ -99,17 +105,24 @@ function bestCoins(){
       console.log('request succed');
       var node = document.createElement("p");
       var text = " ";
-      for(var i=0;i<response.data.length;i++){
-        text+=response.data[i].name +":" + response.data[i].quote.USD.price.toFixed(4) + " / ";
+      for (var i = 0; i < response.data.length; i++) {
+        coins = response;
+        text += response.data[i].name + ":" + response.data[i].quote.USD.price.toFixed(4) + " / ";
         var node1 = document.createElement("li");
-        node1.className = "btn btn-dark mb-2";
+        var node2 = document.createElement("a");
+        node2.className = "btn btn-dark mb-2";
+        node2.id = "coin_"+i;
+        node2.setAttribute('href', "#");
+        node2.setAttribute('onClick','showCoinsInfo('+i+')');
         var textnode1 = document.createTextNode(response.data[i].name);
-        node1.appendChild(textnode1);
+        node2.appendChild(textnode1);
+        node1.appendChild(node2);
         document.getElementById("all_coins").appendChild(node1);
       }
       var textnode = document.createTextNode(text);
       node.appendChild(textnode);
       document.getElementById("coins").appendChild(node);
+      focusable1 = document.querySelectorAll(".btn");
     },
     error: function() {
       console.log("error");
@@ -118,21 +131,47 @@ function bestCoins(){
 
 }
 
-function getPrices($) {
+function getPrices() {
   var resp = $.getJSON('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR');
   resp.done(function(data) {
     $('#Bitcoin').text(data[0].price_eur);
   });
-  var resp = $.getJSON('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR');
-  resp.done(function(data) {
+  var resp1 = $.getJSON('https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR');
+  resp1.done(function(data) {
     $('#Ethereum').text(data[0].price_eur);
   });
-  var resp = $.getJSON('https://api.coinmarketcap.com/v1/ticker/tron/?convert=EUR');
-  resp.done(function(data) {
+  var resp2 = $.getJSON('https://api.coinmarketcap.com/v1/ticker/tron/?convert=EUR');
+  resp2.done(function(data) {
     $('#Tron').text(data[0].price_eur);
   });
-  var resp = $.getJSON('https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/?convert=EUR');
-  resp.done(function(data) {
+  var resp3 = $.getJSON('https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/?convert=EUR');
+  resp3.done(function(data) {
     $('#Bitcoin-Cash').text(data[0].price_eur);
   });
-};
+}
+
+function showCoinsInfo(i){
+  coin = i;
+  document.getElementById("content").style.display = "none";
+  document.getElementById("coin_info").style.display = "block";
+  document.getElementById("return").style.display = "block";
+
+
+  var n = document.createElement("p");
+  textt = coins.data[coin].name + ":" + coins.data[coin].quote.USD.price.toFixed(4);
+  var textn = document.createTextNode(textt);
+  n.appendChild(textn);
+  document.getElementById("coin_info").appendChild(n);
+
+
+
+
+}
+
+function goBack(){
+  document.getElementById("content").style.display = "block";
+  document.getElementById("coin_info").style.display = "none";
+  document.getElementById("return").style.display = "none";
+
+  document.getElementById("coin_info").innerHTML = "";
+}
