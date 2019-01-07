7e894d6a8c379c5581e4351b561ebf505fc6940b
rafarebollo <rafarebollocacin@gmail.com>
2019-01-07 15:14:11 +0100

    chart try

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var init = function() {
     'cache': true
   });
   getPrices();
-  bestCoins();
+  //bestCoins();
   
   refreshRSS();
   document.getElementById("coin_info").style.display = "none";
@@ -69,12 +69,20 @@ var init = function() {
       case 13: //OK button
         document.getElementById(focusable1[i].id).click();
         break;
+        
       case 10009: //RETURN button
         tizen.application.getCurrentApplication().exit();
         break;
+        
+      case 10253: //EXTRA button
+    	  	console.log("tools pressed")
+    	  	break;
+
       default:
         console.log('Key code : ' + e.keyCode);
         break;
+        
+      
     }
   });
 };
@@ -199,9 +207,6 @@ function showCoinsInfo(i){
 
   console.log(coins.data[coin]);
 
-
-
-
 }
 
 function goBack(){
@@ -237,4 +242,48 @@ function refresh(){
   marque.start();
   getPrices();
   bestCoins();
+}
+
+function chart(){
+	var dataPoints = [];
+
+	var chart = new CanvasJS.Chart("chartContainer", {
+		theme: "light2",
+		title: {
+			text: "Live Data"
+		},
+		data: [{
+			type: "line",
+			dataPoints: dataPoints
+		}]
+	});
+	updateData();
+
+	// Initial Values
+	var xValue = 0;
+	var yValue = 10;
+	var newDataCount = 6;
+
+	function addData(data) {
+		if(newDataCount != 1) {
+			$.each(data, function(key, value) {
+				dataPoints.push({x: value[0], y: parseInt(value[1])});
+				xValue++;
+				yValue = parseInt(value[1]);
+			});
+		} else {
+			//dataPoints.shift();
+			dataPoints.push({x: data[0][0], y: parseInt(data[0][1])});
+			xValue++;
+			yValue = parseInt(data[0][1]);
+		}
+		
+		newDataCount = 1;
+		chart.render();
+		setTimeout(updateData, 1500);
+	}
+
+	function updateData() {
+		$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart="+xValue+"&ystart="+yValue+"&length="+newDataCount+"type=json", addData);
+	}	
 }
